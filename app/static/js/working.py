0a28e0d6d7ffa3a9fd8d0ec27723e8d45df92765
josue-acosta <hannibalsway001@gmail.com>
2019-12-14 16:40:34 -0600

    looped thru json file

--- app/static/js/working.py
+++ app/static/js/working.py
@@ -1,19 +1,10 @@
 import json
+import os
 
 json_file_path = "rates.json"
 
 with open(json_file_path, 'r') as j:
-     state_of_good_repair  = json.loads(j.read())
-    
-table_columns = state_of_good_repair["meta"]["view"]["columns"]
+     food_object  = json.loads(j.read())
 
-for column in table_columns:
-    if column["id"] > 0:
-        print("name: " + column["name"])
-        print("description: " + column["description"])
-
-        if len(column["cachedContents"]):
-            for item in column["cachedContents"]["top"]:
-                print(item["count"])
-
-    
+for food_item in food_object:
+    print(food_item)
