39152ae0b2fa27bb7e080e0cb14ffa5676690305
josue-acosta <hannibalsway001@gmail.com>
2019-12-14 12:27:40 -0600

    load json file and parse through main properties

--- app/views_home.py
+++ app/views_home.py
@@ -1,40 +1,47 @@
 from app import app
-
-from flask import render_template
+from flask import render_template, json
+import os
 
 # home pages
-@app.route('/')
+@app.route("/")
 def home():
-    hero_type = 'hero'
-    return render_template('home/home.html', hero_type=hero_type)
+    hero_type = "hero"
+    return render_template("home/home.html", hero_type=hero_type)
 
-@app.route('/engaging-with-flask')
+@app.route("/engaging-with-flask")
 def engaging_with_flask():
-    hero_type = 'hero-advert'
-    return render_template('home/engaging-with-flask.html', hero_type=hero_type)
+    hero_type = "hero-advert"
+    return render_template("home/engaging-with-flask.html", hero_type=hero_type)
 
-@app.route('/switching-between-photos')
+@app.route("/switching-between-photos")
 def switching_between_photos():
-    hero_type = 'hero-advert'
+    hero_type = "hero-advert"
     title = "Switching Between Photos"
-    return render_template('home/switching-between-photos.html',
+    return render_template("home/switching-between-photos.html",
                                 hero_type=hero_type,
                                 title=title)
 
-@app.route('/working-with-json')
+@app.route("/working-with-json")
 def working_with_json():
-    hero_type = 'hero-advert'
+
+    filename = os.path.join(app.static_folder, 'js', 'rates.json')
+    with open(filename) as test_file:
+        data = json.load(test_file)
+    
+    hero_type = "hero-advert"
     title = "Working with JSON"
-    return render_template('home/working-with-json.html',
+
+    return render_template("home/working-with-json.html",
+                                data=data,
                                 hero_type=hero_type,
                                 title=title)
 
 # misc pages
-@app.route('/todo')
+@app.route("/todo")
 def todo():
-    hero_type = 'hero'
-    return render_template('todo.html', hero_type=hero_type)
+    hero_type = "hero"
+    return render_template("todo.html", hero_type=hero_type)
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return 'You took a wrong turn. Go back.', 404
+    return "You took a wrong turn. Go back.", 404
